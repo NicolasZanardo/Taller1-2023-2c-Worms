c85468415aae6cc8b6182e438106c859dd542891
Juan Duzac <juan.duzac@astropay.com>
2023-11-08 09:14:56 -0300

    GameInstance creation GameInstance owns a: 	- PhysicsSystem 	- EntityManager 	- ClientWormsMap (keys: clients, values: list of the worms ids owned by the client) 	- NetQueue (game queue that has NetMessages sent by clients)
    When constructed GameInstance iterate through the clients, and according to the GameScenarioData, spawn worms and assign them to clients

--- server/src/GameInstance.cpp
+++ server/src/GameInstance.cpp
@@ -1,5 +1,109 @@
 #include "GameInstance.h"
 
-GameInstance::GameInstance(const GameScenario& scenario) : clientWormsMap(),
-                                               gameQueue(50),
-                                               physicsSystem(0.0f, -10.0f, scenario) {}
+GameInstance::GameInstance(
+        float xGravity,
+        float yGravity,
+        const GameScenarioData &scenario, // TODO Game config struct
+        const std::list<Client*>& clients
+):  physicsSystem(xGravity, yGravity, scenario),
+    entityManager(physicsSystem),
+    clientWormsMap(),
+    gameQueue(50) {
+    init_game(scenario, clients);
+}
+
+void GameInstance::run() {
+    // TODO LOOP
+    // _process_actions();
+    // _apply_physics()
+    // _update_game() Turn, time, etc
+    // _maintain_connections
+    // _send_game_state()
+    // _synchronize()
+}
+
+void GameInstance::stop() {
+ // TODO Clients back to WaitingLobby
+}
+
+/* PRIVATE */
+
+void GameInstance::init_game(const GameScenarioData &scenario, const std::list<Client *>& clients) {
+    _create_and_assign_worms(scenario, clients);
+    // TODO Send to clients INITIAL MESSAGE (Beam Positions, etc)
+    // TODO Send first update()
+}
+
+void GameInstance::_process_actions() {
+    /* NetMessage* msg = nullptr;
+    while ((msg = gameQueue.pop())) {
+        try {
+            msg->exec(game);
+        } catch () {
+
+        }
+        delete msg;
+    } */
+}
+
+void GameInstance::_create_and_assign_worms(const GameScenarioData &scenario, const std::list<Client *>& clients) {
+    // Calculate the number of clients and the total number of worms
+    size_t numClients = clients.size();
+    size_t numWorms = scenario.worms.size();
+
+    // Calculate the base number of worms per client
+    size_t wormsPerClient = numWorms / numClients;
+
+    // Calculate the number of clients that will have an extra worm
+    size_t clientsWithExtraWorm = numWorms % numClients;
+
+    // Create a list of available worms
+    std::list<int> availableWorms;
+    for (int i = 0; i < numWorms; i++) {
+        availableWorms.push_back(i);
+    }
+
+    // Assign worms to clients
+    for (Client* client : clients) {
+        size_t wormsToAssign = wormsPerClient;
+
+        // Check if this client should have an extra worm
+        if (clientsWithExtraWorm > 0) {
+            wormsToAssign++;
+            clientsWithExtraWorm--;
+        }
+
+        std::list<size_t> assignedWorms;
+        for (int i = 0; i < wormsToAssign; i++) {
+            if (!availableWorms.empty()) {
+                size_t wormId = availableWorms.front();
+                availableWorms.pop_front();
+                WormScenarioData wormScenarioData = scenario.worms[wormsToAssign];
+                entityManager.create_worm(wormScenarioData); // TODO Test out of index
+                assignedWorms.push_back(wormId);
+            }
+        }
+
+        clientWormsMap[client->id] = assignedWorms;
+    }
+
+    // Adjust the health of worms owned by clients with less worms
+    int extraHealth = 25;
+
+    // Iterates thorught the clients lists of worms
+    // If a client has a worm list with less worms than the normal adapt health for all worms in the list
+    for (const auto& entry : clientWormsMap) {
+        size_t clientWormCount = entry.second.size();
+        if (clientWormCount < wormsPerClient) {
+            for (size_t wormId : entry.second) {
+                // Increase the health of worms owned by this client
+                Worm* worm = entityManager.get_worm(wormId);
+                if (worm) {
+                    worm->health += extraHealth;
+                }
+            }
+        }
+    }
+}
+
+
--- server/src/GameInstance.h
+++ server/src/GameInstance.h
@@ -4,18 +4,29 @@
 #include <list>
 #include <unordered_map>
 #include "../../common_base/Networking/Messages/net_queue.h"
+#include "../../common_base/thread.h"
 #include "simulation/PhysicsSystem.h"
+#include "client/client.h"
+#include "model/entities/Worm.h"
+#include "model/entities/EntityManager.h"
 
-typedef std::unordered_map<int, std::list<int>> ClientWormsMap;
+typedef std::unordered_map<int, std::list<size_t>> ClientWormsMap;
 
-class GameInstance {
+class GameInstance: public Thread {
 private:
+    PhysicsSystem physicsSystem;
+    EntityManager entityManager;
     ClientWormsMap clientWormsMap;
     NetQueue gameQueue;
-    PhysicsSystem physicsSystem;
+
+    void init_game(const GameScenarioData& scenario, const std::list<Client*>& clients);
+    void _create_and_assign_worms(const GameScenarioData &scenario, const std::list<Client *>& clients);
+    void _process_actions();
 
 public:
-    explicit GameInstance(const GameScenario& scenario);
+    explicit GameInstance(float xGravity, float yGravity, const GameScenarioData& scenario, const std::list<Client*>& clients);
+    void run() override;
+    void stop() override;
 
 };
 
