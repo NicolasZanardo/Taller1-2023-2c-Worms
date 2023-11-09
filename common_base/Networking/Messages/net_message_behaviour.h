eb0e88f7a9f9e60083f7ee99adc3b475e71d14fe
NicolasZanardo <nzanardo@fi.uba.ar>
2023-11-09 00:49:23 -0300

    Actualizar lugar donde se almacenan las constantes de identificadores de mensajes.

--- common_base/Networking/Messages/net_message_behaviour.h
+++ common_base/Networking/Messages/net_message_behaviour.h
@@ -1,10 +1,19 @@
 #ifndef COMMON_NET_MESSAGE_BEHAVIOUR_H_
 #define COMMON_NET_MESSAGE_BEHAVIOUR_H_
 
+#define NET_MESSAGE_TYPE_TEST 0
+#define NET_MESSAGE_TYPE_CHAT 1
+#define NET_MESSAGE_TYPE_LEAVE 2
+#define NET_MESSAGE_TYPE_INFORM_ID 3
+#define NET_MESSAGE_TYPE_INITIAL_STATE 4
+#define NET_MESSAGE_TYPE_UPDATE_STATE 5
+
 class NetMessageChat;
 class NetMessageLeave;
 class NetMessage_test;
 class NetMessageInformID;
+class NetMessageInitialGameState;
+class NetMessageGameStateUpdate;
 
 class NetMessageBehaviour {
     public:
@@ -12,5 +21,7 @@ class NetMessageBehaviour {
     virtual void run(NetMessageLeave* msg) = 0;
     virtual void run(NetMessage_test* msg) = 0;
     virtual void run(NetMessageInformID* msg) = 0;
+    virtual void run(NetMessageInitialGameState* msg) = 0;
+    virtual void run(NetMessageGameStateUpdate* msg) = 0;
 };
 #endif
