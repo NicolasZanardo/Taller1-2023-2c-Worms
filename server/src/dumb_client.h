7b6552e30144a24774a1f6713e87e6711a68d0fc
NicolasZanardo <nzanardo@fi.uba.ar>
2023-11-11 09:50:20 -0300

    Agregar destructor virtual a net_message_behaviour.h para que se pueda deletear sin castear

--- server/src/dumb_client.h
+++ server/src/dumb_client.h
@@ -28,5 +28,7 @@ class DumbInterpreter : public NetMessageBehaviour {
     virtual void run(NetMessageInformID* msg) override;
     virtual void run(NetMessageInitialGameState* msg) override;
     virtual void run(NetMessageGameStateUpdate* msg) override;
+
+    ~DumbInterpreter() {}
 };
 #endif 
