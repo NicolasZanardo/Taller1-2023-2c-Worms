184beb1266262088c440be225a0212074ac72db2
Juan Duzac <juan.duzac@astropay.com>
2023-11-13 19:37:35 -0300

    Fix: Scenario beams without space in between them on the y:0 level

--- server/src/game/scenario/HardcodedScenarioData.h
+++ server/src/game/scenario/HardcodedScenarioData.h
@@ -12,7 +12,7 @@ public:
         // Populate large beams next to each other at y=0
         for (int i = 0; i < 10; i++) {
             scenario.beams.emplace_back(
-                    i * 2.0f, // x
+                    i * 6.0f + (3), // x
                     0.0f, // y
                     0.0f, // angle
                     BeamScenarioData::Type::LONG
@@ -20,20 +20,20 @@ public:
         }
 
         // Populate large beams at y=1 in the middle of the scenario
-        for (int i = 0; i < 2; i++) {
+        /*for (int i = 0; i < 2; i++) {
             scenario.beams.emplace_back(
                     10.0f + i * 2.0f, // x
                     1.0f, // y
                     0.0f, // angle
                     BeamScenarioData::Type::LONG
             );
-        }
+        }*/
 
         // Populate worms on each side of the map
         scenario.worms.emplace_back(1.0f, 2.5f);
-        scenario.worms.emplace_back(3.0f, 2.5f);
-        scenario.worms.emplace_back(7.0f, 2.5f);
-        scenario.worms.emplace_back(9.0f, 2.5f);
+        // scenario.worms.emplace_back(3.0f, 2.5f);
+        // scenario.worms.emplace_back(7.0f, 2.5f);
+        // scenario.worms.emplace_back(9.0f, 2.5f);
         return scenario;
     }
 };
