8510a83567dd5fc5e6a491bc8b6f004d067f8f57
Nicolas <nzanardo@fi.uba.ar>
2023-11-20 11:26:36 -0300

    correccion de alineamiento para vigas. Falta corregir las que tienen angulo negativo.

--- client/game_ui/client_game_state.cpp
+++ client/game_ui/client_game_state.cpp
@@ -30,7 +30,7 @@ void ClientGameState::load(std::shared_ptr<ClientGameStateDTO> game_state_dto) {
         }
 
         auto image = display.new_sprite("beam_large", beam_w, beam_h, beam.angle);
-        image->set_pos(beam.x, beam.y);
+        image->set_pos(beam.x+0.6f, beam.y+0.1f);
     }
 
     for (auto& worm : game_state_dto->worms) {
--- server/src/game/scenario/HardcodedScenarioData.h
+++ server/src/game/scenario/HardcodedScenarioData.h
@@ -8,16 +8,16 @@ public:
     static GameScenarioData get() {
         GameScenarioData scenario;
         scenario.room_height = 40;
-        scenario.room_width = 36;
+        scenario.room_width = 80;
         // Populate large beams next to each other at y=0
         float i = 0;
-        scenario.beams.emplace_back(i++ * 6.0f + 3.0f, 3.0f, 00.0f, BeamScenarioData::Type::LONG);
-        scenario.beams.emplace_back(i++ * 6.0f + 3.0f, 3.0f, 10.0f, BeamScenarioData::Type::LONG);
-        scenario.beams.emplace_back(i++ * 6.0f + 3.0f, 3.0f, 30.0f, BeamScenarioData::Type::LONG);
-        scenario.beams.emplace_back(i++ * 6.0f + 3.0f, 3.0f, 45.0f, BeamScenarioData::Type::LONG);
-        scenario.beams.emplace_back(i++ * 6.0f + 3.0f, 3.0f, -30.0f, BeamScenarioData::Type::LONG);
-        scenario.beams.emplace_back(i++ * 6.0f + 3.0f, 3.0f, -45.0f, BeamScenarioData::Type::LONG);
-        scenario.beams.emplace_back(i++ * 6.0f + 3.0f, 3.0f, 90.0f, BeamScenarioData::Type::LONG);
+        scenario.beams.emplace_back(i++ * 6.0f + 23.0f, 13.0f, 00.0f, BeamScenarioData::Type::LONG);
+        scenario.beams.emplace_back(i++ * 6.0f + 23.0f, 13.0f, 10.0f, BeamScenarioData::Type::LONG);
+        scenario.beams.emplace_back(i++ * 6.0f + 23.0f, 13.0f, 30.0f, BeamScenarioData::Type::LONG);
+        scenario.beams.emplace_back(i++ * 6.0f + 23.0f, 13.0f, 45.0f, BeamScenarioData::Type::LONG);
+        scenario.beams.emplace_back(i++ * 6.0f + 23.0f, 13.0f, -30.0f, BeamScenarioData::Type::LONG);
+        scenario.beams.emplace_back(i++ * 6.0f + 23.0f, 13.0f, -45.0f, BeamScenarioData::Type::LONG);
+        scenario.beams.emplace_back(i++ * 6.0f + 23.0f, 13.0f, 90.0f, BeamScenarioData::Type::LONG);
 
 
         // Populate large beams at y=1 in the middle of the scenario
@@ -31,7 +31,7 @@ public:
         }*/
 
         // Populate worms on each side of the map
-        scenario.worms.emplace_back(4.0f, 8.0f);
+        scenario.worms.emplace_back(23.0f, 18.0f);
         // scenario.worms.emplace_back(6.0f, 8.0f);
         // scenario.worms.emplace_back(10.0f, 8.0f);
         // scenario.worms.emplace_back(14.0f, 8.0f);
