729bd2733bb64b2bc0ca956abfac697d0d278e7f
Juan Duzac <juan.duzac@astropay.com>
2023-11-23 10:44:34 -0300

    Server: Refactor TurnManager to have ADDITIONAL_TIME state

--- server/src/game/systems/turns/TurnManager.cpp
+++ server/src/game/systems/turns/TurnManager.cpp
@@ -10,6 +10,7 @@ void TurnManager::update(
 ) {
     // Check if the game time has run out
     if (game_time_left <= 0) {
+        // TODO ID IS SIZE_T NOT CORRECT, CHANGE TO INT
         current_client_id = -1;
         current_worm_id = -1;
         return;
@@ -17,27 +18,57 @@ void TurnManager::update(
 
     // Update times
     game_time_left -= it * rate;
-    if (!waiting_to_start_next_turn) {
-        turn_time_left -= it * rate;
-        if (turn_time_left <= 0) {
-            end_actual_turn(active_worm);
+    switch(state) {
+        case TurnState::WAITING_TO_START_NEXT_TURN: {
+            // Only advance to the next turn when worms are still // TODO When explosions finished also
+            if (worms_are_still(worms)) {
+                advance_to_next_turn();
+            }
+            break;
         }
-    } else {
-        // Only advance to the next turn when worms are still // TODO When explosions finished also
-        if (worms_are_still(worms)) {
-            advance_to_next_turn();
+        case TurnState::TURN_TIME: {
+            if (step_turn_time(it, active_worm))
+                break;
+            check_ending_turn_action(active_worm);
+            break;
+        }
+
+        case TurnState::ADDITIONAL_TURN_TIME: {
+            step_turn_time(it, active_worm);
+            break;
         }
     }
 }
 
+// Returns true if the step ended the actual turn, false otherwise
+bool TurnManager::step_turn_time(const int it, const std::shared_ptr<Worm>& active_worm) {
+    turn_time_left -= it * rate;
+    std::cout << "Turn time left: " << turn_time_left << std::endl;
+    if (turn_time_left <= 0) {
+        end_actual_turn(active_worm);
+        return true;
+    }
+    return false;
+}
+
+void TurnManager::check_ending_turn_action(const std::shared_ptr<Worm>& active_worm) {
+    if (active_worm && active_worm->has_done_an_ending_turn_action) {
+        turn_time_left = TIME_AFTER_ENDING_TURN_ACTION;
+        state = TurnState::ADDITIONAL_TURN_TIME;
+    }
+}
+
+
 void TurnManager::end_actual_turn(const std::shared_ptr<Worm>& active_worm) {
-    active_worm->on_turn_ended();
-    waiting_to_start_next_turn = true;
+    if (active_worm) {
+        active_worm->on_turn_ended();
+    }
+    state = TurnState::WAITING_TO_START_NEXT_TURN;
 }
 
 void TurnManager::advance_to_next_turn() {
-    waiting_to_start_next_turn = false;
-    turn_time_left = turn_duration;
+    state = TurnState::TURN_TIME;
+    turn_time_left = TURN_DURATION;
 
     // Find the iterator for the current client
     auto currentClientIterator = clients_ids_to_worms_ids_iterator.find(current_client_id);
@@ -107,7 +138,7 @@ bool TurnManager::is_clients_turn(size_t client_id) const {
 }
 
 int TurnManager::get_current_client_id() const {
-    if (waiting_to_start_next_turn) {
+    if (state == TurnState::WAITING_TO_START_NEXT_TURN) {
         return -1;
     } else {
         return current_client_id;
@@ -115,7 +146,7 @@ int TurnManager::get_current_client_id() const {
 }
 
 int TurnManager::get_current_worm_id() const {
-    if (waiting_to_start_next_turn) {
+    if (state == TurnState::WAITING_TO_START_NEXT_TURN) {
         return -1;
     } else {
         return current_worm_id;
