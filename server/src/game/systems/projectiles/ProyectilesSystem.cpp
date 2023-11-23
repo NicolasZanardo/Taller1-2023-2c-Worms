70b28d8f4ee06b0353306baba7943bebe0dfdbff
Juan Duzac <juan.duzac@astropay.com>
2023-11-23 10:58:20 -0300

    Server: deleted unused ProjectilesSystem

--- server/src/game/systems/projectiles/ProyectilesSystem.cpp
+++
@@ -1,7 +0,0 @@
-#include "ProyectilesSystem.h"
-
-void ProyectilesSystem::update(std::vector<ProjectileInfo>& projectiles) {
-    for (auto projectile: projectiles) {
-       // projectile.update(); TODO Delete system?
-    }
-}
