298b74d4c56edb99f627c47a055989613a556acf
Juan Duzac <juan.duzac@astropay.com>
2023-11-24 21:51:32 -0300

    Server: PhysicsSystem now returns Body class instead of B2Body.

--- server/src/game/systems/physics/PhysicsSystem.h
+++ server/src/game/systems/physics/PhysicsSystem.h
@@ -8,6 +8,7 @@
 #include "../../model/worm/Worm.h"
 #include "../../../../../common_base/constants.h"
 #include "PhysicsCollisionListener.h"
+#include "../../model/projectiles/Projectile.h"
 
 class PhysicsSystem {
 public:
@@ -15,14 +16,20 @@ public:
 
     void update(const std::unordered_map<size_t, std::shared_ptr<Worm>> &worms);
 
-    b2Body *spawn_worm(WormScenarioData wormScenarioData, std::shared_ptr<Worm> wormModel);
-    b2Body* spawn_projectile(const std::shared_ptr<ProjectileInfo>& projectile, float shot_angle);
+    std::unique_ptr<WormBody> spawn_worm(WormScenarioData wormScenarioData, std::shared_ptr<Worm> wormModel);
+
+    std::unique_ptr<ProjectileBody> spawn_projectile(
+        const std::unique_ptr<ProjectileInfo> &projectile_info,
+        float shot_angle,
+        const std::shared_ptr<Projectile> &projectile
+    );
 
 private:
     // Define category bits for fixtures
     const uint16_t WORM_CATEGORY = 0x0001;
     const uint16_t GROUND_CATEGORY = 0x0002;
     const uint16_t PROJECTILE_CATEGORY = 0x0003;
+    const uint16_t EXPLOSION_SENSOR_CATEGORY = 0x0004;
 
     const float timeStep;
     const int32 velocityIterations = 8;
@@ -33,6 +40,7 @@ private:
     void populate_beams(const GameScenarioData &map);
 
     void spawn_beam(BeamScenarioData beam);
+
     std::pair<float, float> angle_to_normalized_vector(float angleDegrees);
 
 };
