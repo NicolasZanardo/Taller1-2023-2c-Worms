ea65f7787a09cd2771eb58a382845b88977b28a9
Nicolas <nzanardo@fi.uba.ar>
2023-11-25 22:10:41 -0300

    Modificar texto de display para que se pueda actualizar.

--- client/game_display/ui_elements/game_text_display.h
+++ client/game_display/ui_elements/game_text_display.h
@@ -18,14 +18,16 @@ class GameTextDisplay : public Displayable {
     float x,y;
     bool absolute;
     std::unique_ptr<SDL2pp::Texture> texture;
-    std::unique_ptr<SDL2pp::Surface> surfaceMessage;
     bool is_hidden;
     TextAlign align;
+    SDL2pp::Font font;
+    SDL2pp::Surface surfaceMessage;
 
     public:
     ~GameTextDisplay() override;
     explicit GameTextDisplay(GameCamera& cam, float x, float y, int fnt_size, TextAlign align, TextLayer layer, const std::string& text);
 
+    void update(const std::string& newval);
     void hidden(bool is_hidden);
     void set_pos(float x, float y);
     void render(SDL2pp::Renderer& renderer, float delta_time) override;
